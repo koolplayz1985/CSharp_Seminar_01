ef2b9f036499cfb3e6f696a0b8c373d6ca6463b2
ILDAR <koolplayz@mail.ru>
2022-12-03 23:16:43 +0300

    Решил первую задачу ДЗ - Найти max из трёх чисел

---
+++ Homework_01/Ex01/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("start");
+
+int a = 44, b = 5, c = 78;
+int max;
+
+if (a > b)
+{
+  max = a;
+}
+else
+{
+  max = b;   
+} 
+if (a > c)
+{
+  max = a;
+}
+else
+{
+  max = c;   
+} 
+if (b > c)
+{
+  max = b;
+}
+else
+{
+  max = c;   
+} 
+
+Console.WriteLine (max);
+Console.WriteLine ("end");
